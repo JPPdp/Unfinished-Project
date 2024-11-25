bb75931e83731de6b0b3c9ae1b7a6ae8f3e4dbd5
James Patrick Paloyo <payloyo@gmail.com>
2024-11-26 07:14:01 +0800

    Test Go

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text("hey low"),
+  home: Scaffold(
+      appBar: AppBar(
 
-));
+        title:Text(
+          'Lets Go!',
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 25.0,
+          ),),
+
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+
+      body: Center(
+        child: Text(
+            'Hello World',
+            style: TextStyle(
+              color: Colors.grey,
+              letterSpacing: 2.0,
+              fontSize: 50.0,
+              fontWeight: FontWeight.bold,
+            ),
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text(
+            'Test',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+        ),
+        backgroundColor: Colors.green,
+      ),
+
+  ),));
